426749eb283c079aff61318cdab40637833d6bab
sahmohseni <mohseni.seyedamirhessam@gmail.com>
2023-02-26 11:47:43 +0330

    first commit

---
+++ nike/lib/theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class LightTheme {
+  static const primaryColor = Colors.black;
+  static const secondaryColor = Colors.grey;
+  static const primaryTextColor = Colors.black;
+  static const secondaryTextColor = Colors.grey;
+}
